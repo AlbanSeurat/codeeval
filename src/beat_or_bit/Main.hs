f1aaa770a7ac55a595ae3821d2b11cd99d91d8d0
Alban Seurat <alban.seurat@me.com>
2016-02-20 21:16:12 +0100

    Create Main.hs

---
+++ src/beat_or_bit/Main.hs
@@ -0,0 +1,43 @@
+module Main where
+
+import System.Environment (getArgs)
+import Data.List (groupBy, elemIndex, intercalate)
+import Data.Function (on)
+import Data.Char (digitToInt)
+import Data.Maybe (listToMaybe)
+import Numeric    (readInt)
+import Data.Bits
+
+main :: IO()
+
+readInput :: IO [String]
+
+readInput = do
+            [fileName] <- getArgs
+            content <- readFile fileName
+            return (lines content)
+
+applyByLine :: (String -> String) -> IO()
+applyByLine func = do
+            lines <- readInput
+            mapM_ putStrLn (map func lines)
+
+split :: String -> String -> [String]
+split s x = filter (not . (split' s)) $ groupBy ((==) `on` (`elem` s)) x
+         where split' n xs = any (`elem` xs) n
+
+
+readBin :: String -> Maybe Int
+readBin = fmap fst . listToMaybe . readInt 2 (`elem` "01") digitToInt
+
+grayToBin :: Int -> Int
+grayToBin 0 = 0
+grayToBin g = g `xor` (grayToBin $ g `shiftR` 1)
+
+grayToBit :: String -> String
+grayToBit x = maybe "error" show $ fmap (\y -> grayToBin y) $ readBin x
+
+beatOrBit x = intercalate " | " $ map grayToBit $ split " |" x
+
+
+main = applyByLine beatOrBit
